7212a3fd510944435708b981be2864893468f80c
allwinjandrews <allwin@labglo.com>
2019-11-22 16:07:20 +0530

    Removes access key and added to .env file

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,13 +1,15 @@
-export const API_URL = process.env.REACT_APP_WEATHER_API_URL;
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-const access_key = "?access_key=38a937e08594d60085940d6dfbe15a36";
+const ACCESS_STRING = `?access_key=${API_KEY}`;
 
-export const LIVE_URL = `${API_URL}live${access_key}`;
+export const API_URL = "http://apilayer.net/api/";
 
-export const HISTORICAL_URL = `${API_URL}historical${access_key}&date=`;
+export const LIVE_URL = `${API_URL}live${ACCESS_STRING}`;
 
-export const CONVERT_URL = `${API_URL}convert${access_key}&from=`;
+export const HISTORICAL_URL = `${API_URL}historical${ACCESS_STRING}&date=`;
 
-export const TIMEFRAME_URL = `${API_URL}timeframe${access_key}&start_date=`;
+export const CONVERT_URL = `${API_URL}convert${ACCESS_STRING}&from=`;
 
-export const CHANGE_URL = `${API_URL}change${access_key}&currencies=`;
+export const TIMEFRAME_URL = `${API_URL}timeframe${ACCESS_STRING}&start_date=`;
+
+export const CHANGE_URL = `${API_URL}change${ACCESS_STRING}&currencies=`;
